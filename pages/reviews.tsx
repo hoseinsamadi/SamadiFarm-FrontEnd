c19fc7d0c5f080d2374781eb427f625123f13198
Hosein Samadi <husein.samadi@gmail.com>
2026-09-08 13:52:39 +0330

    add point of view

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,2 +1,13 @@
 import ReviewsPage from "../src/pages/ReviewsPage";
+import { getReviews } from "../src/lib/reviewsApi";
+
+export const getServerSideProps = async () => {
+  try {
+    const { reviews } = await getReviews();
+    return { props: { reviews } };
+  } catch {
+    return { props: { reviews: [] } };
+  }
+};
+
 export default ReviewsPage;
